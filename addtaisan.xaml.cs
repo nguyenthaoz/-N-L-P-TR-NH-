75e26e48ec278b3ccd773524cc53b7feffbaf374
nguyenthaoz <tn832201@gmail.com>
2025-05-30 11:23:20 +0700

    View

---
+++ addtaisan.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace my2.view
+{
+    /// <summary>
+    /// Interaction logic for addtaisan.xaml
+    /// </summary>
+    public partial class addtaisan : UserControl
+    {
+        public addtaisan()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_dong_Click(object sender, RoutedEventArgs e)
+        {
+            // Ẩn khi nhấn nút
+            this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ lichbaotri.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace my2.view
+{
+    /// <summary>
+    /// Interaction logic for lichbaotri.xaml
+    /// </summary>
+    public partial class lichbaotri : UserControl
+    {
+        public lichbaotri()
+        {
+            InitializeComponent();
+        }
+        private void btn_huylichbaotri_Click(object sender, RoutedEventArgs e)
+        {
+            // Ẩn UserControl khi nhấn nút
+            this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
